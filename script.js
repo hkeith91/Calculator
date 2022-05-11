a8e692479535307c907b731d82d4802635460e41
Hector <hector.keith91@gmail.com>
2022-05-11 16:38:53 -0400

    buttons display values on screen, without commas

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-const numbers = document.querySelectorAll('.number');
-const numbersArr = Array.from(numbers);
-const operations = document.querySelectorAll('.operation');
-const opsArr = Array.from('operations');
+const numbersNL = document.querySelectorAll(".number");
+const numbers = Array.from(numbersNL);
+const operationsNL = document.querySelectorAll(".operation");
+const operations = Array.from("operations");
+const screen = document.querySelector("#screen");
+const buttonsNL = document.querySelectorAll(".btn");
+const buttons = Array.from(buttonsNL);
 
+let screenDisplay;
+let numToDisplay = []
 
+function displayText() {
+  buttons.forEach((button) => {
+    button.addEventListener("click", () =>{
+        numToDisplay.push(button.textContent);
+        screenDisplay = numToDisplay.join("");
+        screen.textContent = screenDisplay;
+    });
+  });
+}
 
+displayText()
