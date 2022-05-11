cb4457d0b5aca62192212a9edd4edf8d4587e236
Hector <hector.keith91@gmail.com>
2022-05-11 09:27:20 -0400

    styled calculator and buttons display proper values

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-generatePlacerholderText();
+// generatePlacerholderText();
 function generatePlacerholderText() {
-    const btns = document.querySelectorAll(".btn");
-    let btnsArr = Array.from(btns);
-    btnsArr.forEach(btn => {
-        btn.textContent = "8";
-    });
+    // const btns = document.querySelectorAll(".btn");
+    // let btnsArr = Array.from(btns);
+    // btnsArr.forEach(btn => {
+    //     btn.textContent = "8";
+    // });
+    
+    const screen = document.querySelector("#screen");
+    screen.textContent = "888888888"
 }
 
