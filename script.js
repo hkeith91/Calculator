6aa33c626c9544faaa120a5b32caf834e7f0e1c3
Hector <hector.keith91@gmail.com>
2022-05-12 06:21:20 -0400

    removed bug

--- script.js
+++ script.js
@@ -4,19 +4,32 @@ const operationsNL = document.querySelectorAll(".operation");
 const operations = Array.from("operations");
 const screen = document.querySelector("#screen");
 const buttonsNL = document.querySelectorAll(".btn");
-const buttons = Array.from(buttonsNL);
+const btns = Array.from(buttonsNL);
+const buttons = btns.forEach(element => {
+  // let
+});
 
 let screenDisplay;
 let numToDisplay = []
 
-function displayText() {
-  buttons.forEach((button) => {
-    button.addEventListener("click", () =>{
-        numToDisplay.push(button.textContent);
-        screenDisplay = numToDisplay.join("");
-        screen.textContent = screenDisplay;
-    });
-  });
-}
+// function displayText() {
+//   buttons.forEach((button) => {
+//     button.addEventListener("click", () =>{
+//         numToDisplay.push(button.textContent);
+//         screenDisplay = numToDisplay.join("");
+//         screen.textContent = screenDisplay;
+//     });
+//   });
+// }
 
-displayText()
+// displayText()
+
+btns.forEach((button) => {
+  button.addEventListener("click", () =>{
+      numToDisplay.push(button.textContent);
+      screenDisplay = numToDisplay.join("");
+      screen.textContent = screenDisplay;
+  });
+});
+
+console.table(btns);
