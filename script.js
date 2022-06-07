8286a5929d8ee1bc5685af6e8a837ca9c386043b
Hector <hector.keith91@gmail.com>
2022-06-07 17:25:26 -0400

    can now store value in numB after entering numA and selecting operator.  Created specOps array for special operations: eaquals, clear etc.

--- script.js
+++ script.js
@@ -1,35 +1,59 @@
 const numbersNL = document.querySelectorAll(".number");
 const numbers = Array.from(numbersNL);
 const operationsNL = document.querySelectorAll(".operation");
-const operations = Array.from("operations");
+const operations = Array.from(operationsNL);
 const screen = document.querySelector("#screen");
 const buttonsNL = document.querySelectorAll(".btn");
 const btns = Array.from(buttonsNL);
-const buttons = btns.forEach(element => {
-  // let
-});
+let numA;
+let numB;
+let currentOperator;
+let enteringNumB = false;
 
 let screenDisplay;
-let numToDisplay = []
+let numToDisplay = [];
 
-// function displayText() {
-//   buttons.forEach((button) => {
-//     button.addEventListener("click", () =>{
-//         numToDisplay.push(button.textContent);
-//         screenDisplay = numToDisplay.join("");
-//         screen.textContent = screenDisplay;
-//     });
-//   });
-// }
+//if operandA == undefined add store value
+//if operandA and operation selected =>
+//  call Operate() when operandB has value and
+//  equals or another operator is added
 
-// displayText()
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    if (currentOperator != undefined && !enteringNumB) {
+      numToDisplay.length = 0;
+      screen.textContent = "";
+      enteringNumB = true;
+    }
+    numToDisplay.push(number.textContent);
+    screenDisplay = numToDisplay.join("");
+    screen.textContent = screenDisplay;
 
-btns.forEach((button) => {
-  button.addEventListener("click", () =>{
-      numToDisplay.push(button.textContent);
-      screenDisplay = numToDisplay.join("");
-      screen.textContent = screenDisplay;
+    numA = undefined
+      ? (numA = parseInt(screenDisplay))
+      : (numB = parseInt(screenDisplay));
   });
 });
 
-console.table(btns);
+operations.forEach((operation) => {
+  operation.addEventListener("click", () => {
+    if (typeof numA !== "number") return;
+
+    if (currentOperator != undefined) {
+      operations.forEach((operation) => {
+        console.log("removing highlight");
+        operation.classList.remove("op-select");
+      });
+    }
+
+    operation.classList.toggle("op-select");
+    if (currentOperator == operation.id) {
+      currentOperator = undefined;
+      console.log("operation = " + currentOperator);
+      return;
+    }
+
+    currentOperator = operation.id;
+    console.log("operation = " + currentOperator);
+  });
+});
